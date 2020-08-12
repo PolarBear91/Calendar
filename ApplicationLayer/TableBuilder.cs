09e5f01906a3e38ef7fd5d643efdfbb12454bcdd
PolarBear91 <stereo.polar.bear@mail.ru>
2020-08-13 00:04:42 +0300

    Added layers and classes

---
+++ ApplicationLayer/TableBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationLayer
+{
+    public class TableBuilder
+    {
+        public static void Build(DateTime dt)
+        {
+            int[] monday = new int[6];
+            int[] tuesday = new int[6];
+            int[] wednesday = new int[6];
+            int[] thursday = new int[6];
+            int[] friday = new int[6];
+            int[] saturday = new int[6];
+            int[] sunday = new int[6];
+
+            int counter = 0;
+            bool isSundayDone = false;
+
+
+            while (counter != 7 || counter < 7)
+            {
+                if ((int)dt.DayOfWeek + counter == 1 || (sunday[0] != 0 && monday[0] == 0))
+                {
+                    Fill(monday, ++counter);
+                }
+
+                if ((int)dt.DayOfWeek + counter == 2 || (sunday[0] != 0 && tuesday[0] == 0))
+                {
+                    Fill(tuesday, ++counter);
+                }
+
+                if ((int)dt.DayOfWeek + counter == 3 || (sunday[0] != 0 && wednesday[0] == 0))
+                {
+                    Fill(wednesday, ++counter);
+                }
+
+                if ((int)dt.DayOfWeek + counter == 4 || (sunday[0] != 0 && thursday[0] == 0))
+                {
+                    Fill(thursday, ++counter);
+                }
+
+                if ((int)dt.DayOfWeek + counter == 5 || (sunday[0] != 0 && friday[0] == 0))
+                {
+                    Fill(friday, ++counter);
+                }
+
+                if ((int)dt.DayOfWeek + counter == 6 || (sunday[0] != 0 && saturday[0] == 0))
+                {
+                    Fill(saturday, ++counter);
+                }
+
+                if (((int)dt.DayOfWeek + counter == 0 || (int)dt.DayOfWeek + counter == 7) && (sunday[0] == 0))
+                {
+                    Fill(sunday, ++counter);
+                    isSundayDone = true;
+                }
+            
+
+            }
+
+        }
+    }
+}
--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using ApplicationLayer;
+using System;
 
 namespace Calendar
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter day:");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter month:");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter year:");
+            int year = int.Parse(Console.ReadLine());
+
+            if (day >= 32 || month >= 13 || (year >= 2400 || year <= 1600))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Not correct date.Please try again... ");
+                Console.ReadKey();
+                Console.Clear();
+                Main();
+            }
+
+            int selectedDay = day;
+            var dt = new DateTime(year, month, 1);
+            DateTime SelectedDt;
+            int countOfDays = System.DateTime.DaysInMonth(dt.Year, dt.Month);
+
+            try
+            {
+                SelectedDt = new DateTime(year, month, day);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+                Console.WriteLine("To continue press enter...");
+                Console.ReadKey();
+                Console.Clear();
+                Main();
+            }
+
+            TableBuilder.Build(dt);
         }
     }
 }
